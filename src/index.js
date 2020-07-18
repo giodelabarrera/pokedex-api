a5d1a28d9ca60b4d1f27e2fc1a8bc728386d5b3f
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-07-18 20:39:59 +0200

    Add data and list endpoint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
+const cors = require('cors')
+const express = require('express')
+// const yup = require('yup')
 
+// const { retrieveSharedSession } = require('./retrieve-shared-session')
+
+const app = express()
+const serverPort = process.env.PORT || 3030
+
+app.use(cors())
+
+app.get('/pokemon', async function (req, res) {
+  // - query?: string (nombre o numero)
+  // - type?: string[] (electrico, bicho, volador) = []
+  // - sort?: string (a-z, z-a, numero superior, numero inferior) = numero superior
+  // - limit?: number (numero de resultados a traer) = 25
+  // - offset?: number (indice de paginacion) = 0
+
+  // req.query -> {list: "20"} || {}
+
+  res.status(200).json({ text: 'list' })
+})
+
+app.get('/pokemon/:idOrName', async function (req, res) {
+  // req.params -> {idOrName: "pikachu"}
+
+  // eslint-disable-next-line
+  debugger
+  res.status(200).json({ text: 'single' })
+})
+
+app.listen(serverPort, function (err) {
+  if (err) throw err
+  console.log(`> Running on ${serverPort}.`)
+})
