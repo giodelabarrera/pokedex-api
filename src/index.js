b02b8ecfecd7ec0ff4cf8a3e0cb7d465de160bb0
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-07-27 02:57:09 +0200

    Add https

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const cors = require('cors')
+const fs = require('fs')
+const https = require('https')
 const express = require('express')
 
 const app = express()
@@ -10,7 +12,15 @@ router.use('/type', require('./type'))
 
 app.use(cors())
 app.use(router)
-app.listen(serverPort, function (err) {
-  if (err) throw err
-  console.log(`> Running on ${serverPort}.`)
-})
+// app.listen(serverPort, function (err) {
+//   if (err) throw err
+//   console.log(`> Running on ${serverPort}.`)
+// })
+
+https.createServer({
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.cert')
+}, app)
+  .listen(serverPort, function () {
+    console.log(`> Running on ${serverPort}.`)
+  })
