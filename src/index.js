95bd89ce0d92de3808c6f10241a8d1c7cc9180b0
Giorgio de la Barrera <giodelabarrera@gmail.com>
2020-07-27 10:23:45 +0200

    Remove https

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 const cors = require('cors')
-const fs = require('fs')
-const https = require('https')
 const express = require('express')
 
 const app = express()
-const serverPort = process.env.PORT || 443
+const serverPort = process.env.PORT || 3030
 
 const router = express.Router()
 router.use('/pokemon', require('./pokemon'))
@@ -13,10 +11,6 @@ router.use('/type', require('./type'))
 app.use(cors())
 app.use(router)
 
-https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app)
-  .listen(serverPort, function () {
-    console.log(`> Running on ${serverPort}.`)
-  })
+app.listen(serverPort, function () {
+  console.log(`> Running on ${serverPort}.`)
+})
